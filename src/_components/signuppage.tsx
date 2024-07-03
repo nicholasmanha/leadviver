56aa89b2d91382b7ee44b3f1d7ab5b6d7e8f0ff6
Michael Bunte <michaelwbunte@gmail.com>
2024-07-03 14:20:28 -0700

    Auth provider buttons

--- src/_components/signuppage.tsx
+++ src/_components/signuppage.tsx
@@ -4,6 +4,11 @@ import Typography from "./Typography"
 import { Button } from "./button"
 import { ReactElement } from "react"
 
+import { ImGoogle2 } from "react-icons/im";
+import { FaFacebookSquare, FaApple, FaMicrosoft } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+
+
 interface SignUpPageProps {
 
 }
@@ -13,10 +18,13 @@ interface AuthProviderProps {
     name?: ReactElement | String;
 }
 
-function AuthProviderButton({logo, name}: AuthProviderProps) {
+function AuthProviderButton({ logo, name }: AuthProviderProps) {
     return <Button className="w-full text-left mb-3">
+        <div className="mr-4" style={{ transform: "scale(1.7)" }}>
+            {logo}
+        </div>
         <div className="w-full">
-            {name}
+            Sign up with {name}
         </div>
     </Button>
 }
@@ -24,14 +32,16 @@ function AuthProviderButton({logo, name}: AuthProviderProps) {
 export default function SignUpPage() {
     return <div>
         <NavbarWrapper />
-        <div className="bg-background opacity-100 lg:w-1/2 sm:w-full min-h-screen flex flex-col ">
-            <div className="bg-primary opacity-100 m-10 px-10 py-5 rounded-xl">
+        <div className="bg-background opacity-100 lg:w-1/2 sm:w-full min-h-screen flex flex-col justify-center">
+            <div className="bg-primary opacity-100 mx-10 px-10 py-5 rounded-xl mb-20">
                 <Typography variant="h3" className='mb-5'>
                     Sign Up
                 </Typography>
-                <AuthProviderButton name="google"/>
-
-
+                <AuthProviderButton logo={<ImGoogle2 />} name="Google" />
+                <AuthProviderButton logo={<FaSquareXTwitter />} name="X" />
+                <AuthProviderButton logo={<FaFacebookSquare />} name="FaceBook" />
+                <AuthProviderButton logo={<FaApple />} name="Apple" />
+                <AuthProviderButton logo={<FaMicrosoft />} name="Microsoft" />
                 <Typography className="text-center mt-6 mb-4">
                     Already have an account? <span className="text-url">Log In</span>
                 </Typography>
