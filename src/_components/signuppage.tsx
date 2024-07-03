a014a60d9bfea798eb18cbf950d6ee10dbee62ac
Michael Bunte <michaelwbunte@gmail.com>
2024-07-03 14:54:14 -0700

    Minor reformatting

--- src/_components/signuppage.tsx
+++ src/_components/signuppage.tsx
@@ -1,9 +1,11 @@
 import NavbarWrapper from "./navbar"
 import Footer from "./footer"
 import Typography from "./Typography"
+import HomeAnimator from "./homeanimator"
+
+
 import { Button } from "./button"
 import { ReactElement } from "react"
-
 import { ImGoogle2 } from "react-icons/im";
 import { FaFacebookSquare, FaApple, FaMicrosoft } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
@@ -32,20 +34,23 @@ function AuthProviderButton({ logo, name }: AuthProviderProps) {
 export default function SignUpPage() {
     return <div>
         <NavbarWrapper />
-        <div className="bg-background opacity-100 lg:w-1/2 sm:w-full min-h-screen flex flex-col justify-center">
-            <div className="bg-primary opacity-100 mx-10 px-10 py-5 rounded-xl mb-20">
-                <Typography variant="h3" className='mb-5'>
-                    Sign Up
-                </Typography>
-                <AuthProviderButton logo={<ImGoogle2 />} name="Google" />
-                <AuthProviderButton logo={<FaSquareXTwitter />} name="X" />
-                <AuthProviderButton logo={<FaFacebookSquare />} name="FaceBook" />
-                <AuthProviderButton logo={<FaApple />} name="Apple" />
-                <AuthProviderButton logo={<FaMicrosoft />} name="Microsoft" />
-                <Typography className="text-center mt-6 mb-4">
-                    Already have an account? <span className="text-url">Log In</span>
-                </Typography>
+        <div className="flex w-full bg-background-padding">
+            <div className="bg-background opacity-100 lg:w-1/2 sm:w-full min-h-screen flex flex-col justify-center">
+                <div className="bg-primary opacity-100 mx-10 px-10 py-5 rounded-xl mb-20">
+                    <Typography variant="h3" className='mb-5'>
+                        Sign Up
+                    </Typography>
+                    <AuthProviderButton logo={<ImGoogle2 />} name="Google" />
+                    <AuthProviderButton logo={<FaSquareXTwitter />} name="X" />
+                    <AuthProviderButton logo={<FaFacebookSquare />} name="FaceBook" />
+                    <AuthProviderButton logo={<FaApple />} name="Apple" />
+                    <AuthProviderButton logo={<FaMicrosoft />} name="Microsoft" />
+                    <Typography className="text-center mt-6 mb-4">
+                        Already have an account? <span className="text-url">Log In</span>
+                    </Typography>
+                </div>
             </div>
+            <HomeAnimator />
         </div>
         <Footer />
     </div>
