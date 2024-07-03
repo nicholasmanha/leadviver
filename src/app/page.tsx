8b01d5ea6b80ea9180b28eb4ead3d77c0f4ed71a
Nicholas Manha <nickrmanha@gmail.com>
2024-07-03 12:08:32 -0700

    pnpm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,27 +8,9 @@ export default function Home() {
 
   return (
     <>
-      <div className="inline-flex flex-col gap-2">
-        
-          <Button variant="default" asChild>
-            <a href="#">Upload Leads</a>
-          </Button>
-        
 
-        <Button variant="default" asChild>
-          <a href="#"><LuUpload className="mr-1" />Upload CSV</a>
-        </Button>
-        
-        <Button variant="default" asChild>
-          <a href="#"><LuDownload className="mr-1" /> <Typography variant="button-4">Download Template CSV</Typography> </a>
-        </Button>
-
-        <Button variant="red" size="lg" color="secondary" asChild>
-          <a href="#"><Typography variant="button-1" >Selling Leads</Typography> </a>
-        </Button>
-        
-      </div>
-      <Typography variant="button-4">Download Template CSV</Typography>
+      <Typography variant="p">Landing Page</Typography>
+    
     </>
   );
 }
