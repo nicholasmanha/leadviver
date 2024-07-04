e1485c78b1522e4261607f936961793ad2c48d9c
Nicholas Manha <nickrmanha@gmail.com>
2024-07-04 12:38:25 -0700

    made tile component

--- src/_components/tile/tile.tsx
+++ src/_components/tile/tile.tsx
@@ -4,43 +4,54 @@ import * as React from "react"
 import './tile.css';
 import { LuChevronUp } from "react-icons/lu";
 import { LuChevronDown } from "react-icons/lu";
-import { useState, createContext, useContext, ReactNode } from 'react';
+import { ReactNode } from 'react';
 
 
 import {
-    Collapsible,
-    CollapsibleContent,
-    CollapsibleTrigger,
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
 } from "./Collapsible"
+import Typography from "../Typography";
 
 interface CollapsibleProps {
-    children?: any
-  }
-
-export function CollapsibleDemo({children}: CollapsibleProps) {
-    const [isOpen, setIsOpen] = React.useState(false)
-
-    return (
-        <Collapsible
-            open={isOpen}
-            onOpenChange={setIsOpen}
-            className="w-full bg-secondary rounded-md"
-        >
-            <div className="flex items-center justify-between space-x-4 px-4 py-2">
-                <h4 className="text-sm font-semibold">
-                    house on da market
-                </h4>
-                <CollapsibleTrigger asChild>
-                    {isOpen ? <LuChevronDown /> : <LuChevronUp />}
-                </CollapsibleTrigger>
-            </div>
-
-            <CollapsibleContent className="CollapsibleContent bg-secondary-light rounded-b-md">
-                <div className="px-4 py-2 font-mono text-sm shadow-sm">
-                    {children}
-                </div>
-                
-            </CollapsibleContent>
-        </Collapsible>
-    )
+  left: ReactNode
+  right?: ReactNode
+  children?: any
 }
+
+
+
+function Tile({ children, left, right }: CollapsibleProps) {
+  const [isOpen, setIsOpen] = React.useState(false)
+
+  return (
+    <Collapsible
+      open={isOpen}
+      onOpenChange={setIsOpen}
+      className="w-full bg-secondary rounded-md"
+    >
+      <div className="flex items-center justify-between space-x-4 px-4 py-2">
+        <div className="w-full">
+          <div className="inline-flex"><Typography>{left}</Typography></div>
+          <div className="float-right inline-flex"><Typography>{right}</Typography></div>
+        </div>
+
+        <CollapsibleTrigger asChild>
+          {isOpen ? <LuChevronDown /> : <LuChevronUp />}
+        </CollapsibleTrigger>
+      </div>
+
+      <CollapsibleContent className="CollapsibleContent bg-secondary-light rounded-b-md">
+        <div className="px-4 py-2 font-mono text-sm shadow-sm">
+          {children}
+        </div>
+
+      </CollapsibleContent>
+    </Collapsible>
+  )
+}
+
+
+
+export default Tile;
--- src/app/sdash/page.tsx
+++ src/app/sdash/page.tsx
@@ -1,13 +1,31 @@
-import {CollapsibleDemo} from "@/_components/tile/tile"
+import Tile from "@/_components/tile/tile"
 import BasePage from "@/_components/basepage";
 import NavbarWrapper from "@/_components/navbar";
+import Typography from "@/_components/Typography";
 
 export default function Page() {
+    const left =
+        <>
+            <div className="flex flex-col">
+                <Typography variant="p-bold" color="secondary">816 Lakeview Boulevard</Typography>
+                <div>
+                    <Typography className="inline-block" variant="h3">$</Typography>
+                    <Typography className="inline-block" variant="h5">150k</Typography>
+                </div>
+            </div>
+
+
+
+        </>
+
     return <div>
         <NavbarWrapper></NavbarWrapper>
         <BasePage>
-            <CollapsibleDemo>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit officiis, obcaecati qui rerum laudantium ad delectus totam sunt blanditiis eaque deleniti error, commodi magni hic laborum adipisci culpa, cum quidem!</CollapsibleDemo>
+
+            <Tile left={left} right={left}>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, magni ullam? Quas quasi, expedita quod quibusdam, laudantium veritatis placeat, in nisi perspiciatis recusandae dolorum magnam doloremque consequuntur quidem dolorem incidunt?
+            </Tile>
         </BasePage>
-        
+
     </div>
 }
