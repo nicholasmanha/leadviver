f4429d369ceecae846f013f759af5300ad0a8e4a
Michael Bunte <michaelwbunte@gmail.com>
2024-07-04 13:36:53 -0700

    Decent looking sign in slideshow

--- src/_components/signuppage.tsx
+++ src/_components/signuppage.tsx
@@ -3,20 +3,9 @@
 import NavbarWrapper from "./navbar"
 import Footer from "./footer"
 import Typography from "./Typography"
-import HomeAnimator from "./homeanimator"
-import {
-    CarouselNext,
-    CarouselPrevious,
-    Carousel,
-    type CarouselApi,
-    CarouselContent,
-    CarouselItem
-} from "./Carousel"
-import { useEffect, useState, useRef, useLayoutEffect, useCallback } from "react";
-import Image from "next/image";
 
-import Autoplay from "embla-carousel-autoplay"
-
+import "./signuppagecss.css"
+import { useRef, useEffect, useState } from "react";
 import { Button } from "./button"
 import { ReactElement } from "react"
 import { ImGoogle2 } from "react-icons/im";
@@ -44,57 +33,75 @@ function AuthProviderButton({ logo, name }: AuthProviderProps) {
     </Button>
 }
 
-interface ImageComponentInterface {
-    reff?: any
+interface ImageComponentProps {
+    filename: string;
 }
 
-function ImageComponent({ reff }: ImageComponentInterface) {
-    return <div style={{
-        display: "inline-block",
-        background: "green",
-        height: "100%"
+function ImageComponent({ filename }: ImageComponentProps) {
 
-    }}>
-        <img
-            ref={reff} 
-            src="/signup_home_imgs/house1.jpg"
-            alt="a suburbian home"
-            style={{ width: 'auto', height: '100%' }} // optional
-        />
+    return <div className="image_slider w-40 z-0"
+        style={{
+            backgroundImage: `url("/signup_home_imgs/${filename}")`,
+            backgroundSize: "cover"
+        }}
+    >
     </div>
 }
 
+const house_filenames = [
+    "house1.jpg",
+    "house2.jpg",
+    "house3.jpg",
+    "house4.jpg",
+    "house5.jpg",
+    "house6.jpg",
+]
+
 export default function SignUpPage() {
-    const final_house_ref = useRef<HTMLDivElement>(null);
-    const handleClick = () => {
-        final_house_ref.current?.scrollIntoView({ behavior: 'smooth', inline: 'center' });
-        final_house_ref.current?.scrollTo({
-            top: 100,
-            left: 100,
-            behavior: "smooth",
-          });
-    };
+    const [images, setImages] = useState([<ImageComponent key="supercool" filename="house1.jpg" />]);
+
+    const [curr_image_num, set_curr_image_num] = useState(2);
+    function increment_curr_image() {
+
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setImages(prev => {
+                const new_arr = [...prev];
+                new_arr.length > 4 && new_arr.shift();
+                new_arr.push(<ImageComponent key={new Date().toISOString()} filename={house_filenames[curr_image_num]} />);
+                return new_arr;
+            });
+            set_curr_image_num(prev => {
+                if (prev >= house_filenames.length) {
+                    return 0
+                }
+                return prev + 1;
+            })
+        }, 8000);
+
+        return () => clearInterval(interval);
+    }, [curr_image_num]);
 
     return <div >
         <NavbarWrapper />
-        <div className="h-20 w-20 bg-pink" onClick={handleClick}>
-            hello
-        </div>
-        <div className=" min-h-screen flex w-full bg-background-padding z-0 relative" >
-            <div className="absolute top-0 left-0 h-full" >
-                <div style={{
-                    overflowX: "hidden",
-                    whiteSpace: "nowrap",
-                    height: "100%"
-                }}>
-                    <ImageComponent />
-                    <ImageComponent />
-                    <ImageComponent />
-                    <ImageComponent />
-                    <ImageComponent reff={final_house_ref} />
-                </div>
+        <div className="min-h-screen flex w-full bg-background-padding z-0 relative" >
+            <div
+                className="absolute w-full h-full"
+            >
+                {images}
             </div>
-            <div className="absolute top-0 left-0 bg-background opacity-100 lg:w-1/2 sm:w-full min-h-screen flex flex-col justify-center">
+            <div
+                className="absolute w-full h-full"
+                style={{
+                    background: "#1a1e37",
+                    opacity: "0.5"
+                }}
+            >
+                hello
+            </div>
+            <div className="absolute top-0 left-0 bg-background opacity-95 lg:w-1/2 sm:w-full min-h-screen flex flex-col justify-center">
                 <div className="bg-primary opacity-100 mx-10 px-10 py-5 rounded-xl mb-20">
                     <Typography variant="h3" className='mb-5'>
                         Sign Up
