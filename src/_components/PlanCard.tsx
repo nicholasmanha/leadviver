35a0658db31e40661280640a7d0b0ea25575155d
Nicholas Manha <nickrmanha@gmail.com>
2024-07-04 13:42:33 -0700

    separated plans page into plan card and page

---
+++ src/_components/PlanCard.tsx
@@ -0,0 +1,32 @@
+import Typography from "./Typography"
+import { Button } from "./button"
+import { ReactElement } from "react"
+
+
+interface PlanCardProps {
+  plan?: ReactElement | String;
+  price?: ReactElement | String;
+  children?: any
+}
+
+export default function PlanCard({ plan, price, children }: PlanCardProps) {
+  return <div className="bg-primary w-full mx-4 rounded-2xl flex flex-col justify-center p-4">
+    <div className="text-center">
+      <Typography variant="h3">{plan} Plan</Typography>
+      <div>
+        <Typography className="inline" variant="h1">{price}</Typography>
+        <Typography className="inline" variant="h3" color="secondary">/mo</Typography>
+      </div>
+    </div>
+
+    <div className="w-full px-8">
+      <div className="float-left">
+        <Typography variant="h4">Features</Typography>
+        <Typography className="inline" variant="p">{children}</Typography>
+      </div>
+    </div>
+    <div className="flex justify-center">
+      <Button variant="default" size="lg" className="mt-8 w-44"><Typography variant="h3">Subscribe</Typography></Button>
+    </div>
+  </div>
+}
--- src/app/payment-plan/page.tsx
+++ src/app/payment-plan/page.tsx
@@ -1,13 +1,43 @@
-import PaymentPlanPage from "@/_components/paymentPlansPage"
+import PaymentPlanPage from "@/_components/PlanCard"
 import BasePage from "@/_components/basepage";
 import NavbarWrapper from "@/_components/navbar";
+import Typography from "@/_components/Typography";
+import { Input } from "@/_components/input";
+import PlanCard from "@/_components/PlanCard";
 
 export default function Page() {
     return <div>
         <NavbarWrapper></NavbarWrapper>
         <BasePage>
-            <PaymentPlanPage/>
+            <div>
+
+                <div className="sm:w-full h-screen min-h-screen flex flex-col justify-center py-40">
+
+                    <Typography variant="h3" className='text-center mb-2'>
+                        Choose what works for you
+                    </Typography>
+
+                    <Typography variant="subtitle" color="secondary" className='text-center mb-5'>
+                        Start with entering a zip code to see rates
+                    </Typography>
+                    <div className="flex justify-center ">
+                        <Input className="w-40 mb-2" type="text" placeholder="Zip"></Input>
+                    </div>
+
+                    <div className="flex justify-evenly my-4 flex-1">
+                        <PlanCard plan="Basic" price="349">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos corrupti magni tenetur nam voluptatibus eligendi qui temporibus aliquam aut, </PlanCard>
+                        <PlanCard plan="Silver" price="589">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos corrupti magni tenetur nam voluptatibus eligendi qui temporibus aliquam aut, </PlanCard>
+                        <PlanCard plan="Gold" price="999">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos corrupti magni tenetur nam voluptatibus eligendi qui temporibus aliquam aut, </PlanCard>
+
+                    </div>
+
+
+
+
+                </div>
+
+            </div>
         </BasePage>
-        
+
     </div>
 }
