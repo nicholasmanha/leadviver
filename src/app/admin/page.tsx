e00eaf6556866b34b3f17375c9d74e8c3acbb5ea
Nicholas Manha <nickrmanha@gmail.com>
2024-07-10 10:34:39 -0700

    added tabs to admin dash

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -17,6 +17,13 @@ import {
 import { Button } from "@/_components/button";
 import { LuDownload } from "react-icons/lu";
 import { LuPlusCircle } from "react-icons/lu";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/_components/Tab"
+
 const invoices = [
   {
     invoice: "INV001",
@@ -62,62 +69,72 @@ const invoices = [
   },
 ]
 
+const TableInstance: React.FC = () => {
+  return (
+
+    <Table>
+
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Name</TableHead>
+          <TableHead>Home Address</TableHead>
+          <TableHead>Phone Number</TableHead>
+          <TableHead className="text-right">Additional Notes</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {invoices.map((invoice) => (
+          <TableRow key={invoice.invoice}>
+            <TableCell className="font-medium">{invoice.invoice}</TableCell>
+            <TableCell>{invoice.paymentStatus}</TableCell>
+            <TableCell>{invoice.paymentMethod}</TableCell>
+            <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+
+    </Table>
+
+  );
+};
+
+
 export default function Page() {
-
-
-
   return <>
     <NavbarWrapper></NavbarWrapper>
     <BasePage>
       <Typography className="text-center" variant="h2">Admin Dashboard</Typography>
       <Card title="User Database">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[100px]">Name</TableHead>
-              <TableHead>Home Address</TableHead>
-              <TableHead>Phone Number</TableHead>
-              <TableHead className="text-right">Additional Notes</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {invoices.map((invoice) => (
-              <TableRow key={invoice.invoice}>
-                <TableCell className="font-medium">{invoice.invoice}</TableCell>
-                <TableCell>{invoice.paymentStatus}</TableCell>
-                <TableCell>{invoice.paymentMethod}</TableCell>
-                <TableCell className="text-right">{invoice.totalAmount}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
+        <Tabs defaultValue="buyers" >
+          <TabsList className="w-[400px]">
+            <TabsTrigger className="mr-3" value="buyers"><Typography variant="h5">Buyers</Typography></TabsTrigger>
+            <TabsTrigger className="mr-3" value="sellers"><Typography variant="h5">Sellers</Typography></TabsTrigger>
+            <TabsTrigger className="mr-3" value="banned"><Typography variant="h5">Banned Users</Typography></TabsTrigger>
 
-        </Table>
+            <TabsTrigger className="mr-3" value="admins"><Typography variant="h5">Admins</Typography></TabsTrigger>
 
+          </TabsList>
+          <TabsContent value="buyers">
+            <TableInstance />
+
+          </TabsContent>
+          <TabsContent value="sellers">
+            <TableInstance />
+
+          </TabsContent>
+          <TabsContent value="banned">
+            <TableInstance />
+
+          </TabsContent>
+          <TabsContent value="admins">
+            <TableInstance />
+
+          </TabsContent>
+        </Tabs>
       </Card>
 
       <Card title="Unassigned Leads" >
-        <Table>
-
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[100px]">Name</TableHead>
-              <TableHead>Home Address</TableHead>
-              <TableHead>Phone Number</TableHead>
-              <TableHead className="text-right">Additional Notes</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {invoices.map((invoice) => (
-              <TableRow key={invoice.invoice}>
-                <TableCell className="font-medium">{invoice.invoice}</TableCell>
-                <TableCell>{invoice.paymentStatus}</TableCell>
-                <TableCell>{invoice.paymentMethod}</TableCell>
-                <TableCell className="text-right">{invoice.totalAmount}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-
-        </Table>
+        <TableInstance />
 
       </Card>
     </BasePage>
