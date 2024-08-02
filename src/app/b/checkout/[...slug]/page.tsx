bbfd030596153a63854adfb4844d19794cdaa67d
Nicholas Manha <nickrmanha@gmail.com>
2024-08-02 09:01:11 -0700

    uses axios in stripe checkout api call

--- src/app/b/checkout/[...slug]/page.tsx
+++ src/app/b/checkout/[...slug]/page.tsx
@@ -1,10 +1,12 @@
 "use client"
-import React, { useCallback} from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { loadStripe, Stripe } from '@stripe/stripe-js';
 import {
   EmbeddedCheckoutProvider,
   EmbeddedCheckout
 } from '@stripe/react-stripe-js';
+import { useParams } from 'next/navigation';
+import axios from 'axios';
 
 const stripePublicKey = process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string;
 if (!stripePublicKey) {
@@ -14,36 +16,52 @@ if (!stripePublicKey) {
 const stripePromise: Promise<Stripe | null> = loadStripe(stripePublicKey);
 
 const CheckoutForm = () => {
-  const fetchClientSecret = useCallback(() => {
-    
-    // Create a Checkout Session
-    return fetch("https://j4y10kfape.execute-api.us-west-1.amazonaws.com/Prod/create-checkout-session", {
-      method: "POST",
-    })
-      .then((res) => res.json())
-      .then((data) => data.clientSecret)
-      
-  },
-    []);
+  const params = useParams();
+  const { slug } = params;
 
 
-  const options = { fetchClientSecret };
+  // Create a Checkout Session
+  const [clientSecret, setClientSecret] = useState('');
+
+  useEffect(() => {
+    if (slug) {
+      console.log(slug);
+      const fetchPaymentIntent = async () => {
+        try {
+          const response = await axios.post('https://j4y10kfape.execute-api.us-west-1.amazonaws.com/Prod/create-checkout-session', {
+            product_id: slug,
+          });
+
+          const { clientSecret } = response.data;
+
+          if (clientSecret) {
+            setClientSecret(clientSecret);
+          }
+        } catch (error) {
+          console.error('Error creating payment intent:', error);
+        }
+      };
+
+      fetchPaymentIntent();
+    }
+  }, [slug]);
 
   return (
     <div id="checkout">
       <EmbeddedCheckoutProvider
         stripe={stripePromise}
-        options={options}
+        options={{clientSecret}}
       >
         <EmbeddedCheckout />
       </EmbeddedCheckoutProvider>
     </div>
   )
+
 };
 
 function Page() {
   return (
-    <CheckoutForm/>
+    <CheckoutForm />
   )
 }
 
