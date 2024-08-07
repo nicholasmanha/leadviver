f22a739f58ffd7ef7fd2ca5caa3614c772dc4ead
Nicholas Manha <nickrmanha@gmail.com>
2024-08-07 15:29:02 -0700

    leads are deleted when unfilled

--- src/app/s/dash/leads.tsx
+++ src/app/s/dash/leads.tsx
@@ -84,23 +84,43 @@ export default function Leads() {
   ]);
 
   useEffect(() => {
-    const allFilled = leads.every(lead => lead.price !== '' && lead.address && lead.date && lead.notes);
-    if (allFilled) {
+    const allFieldsFilled = leads[leads.length - 1].price !== '' && leads[leads.length - 1].address && leads[leads.length - 1].date && leads[leads.length - 1].notes
+    // const allFilled = leads.every(lead => lead.id === Math.max(...leads.map(lead => lead.id)) && lead.price !== '' && lead.address && lead.date && lead.notes);
+    if (allFieldsFilled) {
+
+      // if leads.length != 0, then find the max id and add 1, otherwise make it 1
       const newId = leads.length ? Math.max(...leads.map(lead => lead.id)) + 1 : 1;
+
+      // add new lead to leads array
       setLeads([...leads, { id: newId, price: '', address: '', date: '', notes: '', addable: true }]);
     }
+    
+    leads.forEach(lead => {
+      // if the leads list doesnt just contain 1 lead and if the current lead isnt the last and all fields are empty
+      if(leads.length !== 1 && leads.indexOf(lead) !== leads.length-1 && lead.price === '' && !lead.address && !lead.date && !lead.notes){
+        setLeads(leads.filter(leadInst => leadInst.id !== lead.id));
+      }
+    });
+    
   }, [leads]);
 
   const handleInputChange = (id: number, field: keyof Lead, value: string) => {
+    
     setLeads(leads.map(lead =>
+      // if curr lead id is equal to the id of the input, then check if the field is the 
+      // price then check if the value is empty then make it a string and if not then turn 
+      // it into a number. If the field isnt the price field then just return the value,
+      // if the lead id isnt equal to the lead id, then just return the lead
       lead.id === id
         ? { ...lead, [field]: field === 'price' ? (value === '' ? '' : Number(value)) : value }
         : lead
     ));
+    
+    
   };
 
   const outputToConsole = () => {
-    console.log(leads);
+    console.log(leads.slice(0, -1));
   };
 
   return <>
@@ -120,10 +140,10 @@ export default function Leads() {
 
           {leads.map(lead => (
             <TableRow key={lead.id}>
-              <TableCell className="font-medium"><Input placeholder="price" onChange={(e) => handleInputChange(lead.id, 'price', e.target.value)}></Input></TableCell>
-              <TableCell><Input placeholder="address" onChange={(e) => handleInputChange(lead.id, 'address', e.target.value)}></Input></TableCell>
-              <TableCell><Input placeholder="date" onChange={(e) => handleInputChange(lead.id, 'date', e.target.value)}></Input></TableCell>
-              <TableCell className="text-right"><Input placeholder="notes" onChange={(e) => handleInputChange(lead.id, 'notes', e.target.value)}></Input></TableCell>
+              <TableCell className="font-medium"><Input placeholder="Enter price" onChange={(e) => handleInputChange(lead.id, 'price', e.target.value)} required></Input></TableCell>
+              <TableCell><Input placeholder="Enter address" onChange={(e) => handleInputChange(lead.id, 'address', e.target.value)}></Input></TableCell>
+              <TableCell><Input placeholder="Enter Date" onChange={(e) => handleInputChange(lead.id, 'date', e.target.value)}></Input></TableCell>
+              <TableCell className="text-right"><Input placeholder="Enter notes" onChange={(e) => handleInputChange(lead.id, 'notes', e.target.value)}></Input></TableCell>
             </TableRow>
           ))
 
