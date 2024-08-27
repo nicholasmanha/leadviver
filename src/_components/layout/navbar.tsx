36b77485cb435a1469e72a14a29560595b24ff27
Nicholas Manha <nickrmanha@gmail.com>
2024-08-26 20:30:38 -0700

    fixed payment card positioning

--- src/_components/layout/navbar.tsx
+++ src/_components/layout/navbar.tsx
@@ -1,15 +1,32 @@
-import Link from "next/link"
-import Typography from "@/_components/ui/Typography"
+import Link from "next/link";
+import Typography from "@/_components/ui/Typography";
 
 interface NavbarWrapperProps {
-    children?: any
+  children?: any;
+  absolute?: boolean;
 }
 
-export default function NavbarWrapper({ children }: NavbarWrapperProps) {
-    return <div className="bg-navfoot h-14 flex items-center z-50">
-        <Typography variant="h4" className=" text-text-primary mx-8">
+export default function NavbarWrapper({
+  children,
+  absolute=false,
+}: NavbarWrapperProps) {
+  if (absolute) {
+    return (
+        <div className="absolute bg-navfoot inset-x-0 h-14 flex items-center z-50">
+          <Typography variant="h4" className=" text-text-primary mx-8">
             <Link href="/">LeadViver.com</Link>
+          </Typography>
+          {children}
+        </div>
+      );
+  } else {
+    return (
+      <div className="bg-navfoot h-14 flex items-center z-50">
+        <Typography variant="h4" className=" text-text-primary mx-8">
+          <Link href="/">LeadViver.com</Link>
         </Typography>
         {children}
-    </div>
+      </div>
+    );
+  }
 }
